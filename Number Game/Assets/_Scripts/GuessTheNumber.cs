7b5e73ebc9403dbfe3d2783b991c435aeab07c8e
theonlyoxtale <theonlyoxtale@gmail.com>
2017-11-10 00:55:11 -0500

    created a simple guess the number game

---
+++ Number Game/Assets/_Scripts/GuessTheNumber.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GuessTheNumber : MonoBehaviour {
+
+    public InputField input;
+    public Text infoText;
+
+    private int guessNumber;
+    private int userGuess;
+
+	// Use this for initialization
+	void Start () {
+        guessNumber = Random.Range(0, 100);
+	}
+	
+    public void CheckGuess()
+    {
+        userGuess = int.Parse(input.text);
+
+        if (userGuess == guessNumber)
+        {
+            infoText.text = "You guessed the number";
+        }
+        if (userGuess > guessNumber)
+        {
+            infoText.text = "Your guess is too high";
+        }
+        if (userGuess < guessNumber)
+        {
+            infoText.text = "Your guess is too low";
+        }
+
+        input.text = "";
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
