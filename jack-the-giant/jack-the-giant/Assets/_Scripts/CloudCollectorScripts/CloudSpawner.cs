09b761c369f721874567f8fa6850d30961cc5fd9
theonlyoxtale <theonlyoxtale@gmail.com>
2017-11-18 22:18:00 -0500

    Create the cloud collector
    Cloud collector includes a cloud collector gameobject and cloud spawner
    gameobject and added two scripts for each gameobject to handle
    collecting the clouds and spawning them.

---
+++ jack-the-giant/jack-the-giant/Assets/_Scripts/CloudCollectorScripts/CloudSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour {
+
+    [SerializeField] private GameObject[] clouds;
+
+    // Distance on the y axis between the clouds
+    private float distanceBetweenClouds = 3f;
+
+    // these variables make sure that the clouds do not get placed outside of the camera on the x axis
+    private float minX, maxX;
+
+    // holds the last clouds y axis position
+    private float lastCloudPosY;
+
+    // control the x position of the clouds to make the cloud position random
+    private float controlX;
+
+    [SerializeField] private GameObject[] collectible;
+
+    // used to position the player above the first cloud
+    private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
