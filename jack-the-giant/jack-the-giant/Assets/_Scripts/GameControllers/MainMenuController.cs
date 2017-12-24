e2610ae7ab3bdbf82a48cae6703ea76668455479
theonlyoxtale <theonlyoxtale@gmail.com>
2017-12-23 20:10:10 -0500

    added scene loading code and objects

---
+++ jack-the-giant/jack-the-giant/Assets/_Scripts/GameControllers/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void StartGame()
+    {
+        SceneManager.LoadScene("v0");
+    }
+
+    public void HighScoreMenu()
+    {
+        SceneManager.LoadScene("HighScore");
+    }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MusicButton()
+    {
+        
+    }
+
+}
